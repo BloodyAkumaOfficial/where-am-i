ebe3d7c9dbec4eb994fdafe32a41481795ff5ea0
Denys <111761778+BloodyAkumaOfficial@users.noreply.github.com>
2022-12-25 23:55:57 +0300

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -1,14 +1,19 @@
 const anserBlock = document.querySelector('.answer');
+const img = document.querySelector('img');
 
 const getUserCountry = async function () {
-    let userPosition = await getUserPosition();
-    let {latitude: lt, longitude: ln} = userPosition.coords;
-    let responsePosition = await fetch(`https://geocode.xyz/${lt},${ln}?geoit=json`);
-    let dataPosition = await responsePosition.json();
-    let responseCounry = await fetch(`https://restcountries.com/v3.1/name/${dataPosition.country.toLowerCase()}`);
-    let dataCountry = await responseCounry.json();
-    console.log(dataCountry);
-    anserBlock.innerHTML =  dataCountry[0].altSpellings[0];
+    try {
+        let userPosition = await getUserPosition();
+        let { latitude: lt, longitude: ln } = userPosition.coords;
+        let responsePosition = await fetch(`https://geocode.xyz/${lt},${ln}?geoit=json`);
+        let dataPosition = await responsePosition.json();
+        let responseCounry = await fetch(`https://restcountries.com/v3.1/name/${dataPosition.country.toLowerCase()}`);
+        let dataCountry = await responseCounry.json();
+        img.setAttribute('src', dataCountry[0].flags.svg)
+        anserBlock.innerHTML = dataCountry[0].altSpellings[0];
+    } catch(e) {
+        anserBlock.innerHTML = 'Что-то пошло не так';
+    }
 }
 
 const getUserPosition = function () {
